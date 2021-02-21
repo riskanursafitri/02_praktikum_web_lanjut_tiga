2134617a77db50d09bfb974c03b4d45720ddbfd1
riskanursafitri <safitririska010@gmail.com>
2021-02-21 11:37:15 +0700

    praktikum 3

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,58 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//router biasa
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('https://www.educastudio.com');
+ 
 });
+
+//router prefix
+Route::prefix('/product')->group(function () {
+    Route::get('/marbel-edu-games', function () {
+        return redirect('https://www.educastudio.com/category/marbel-edu-games');
+    });
+    Route::get('/marbel-and-friends-kids-games', function () {
+        return redirect('https://www.educastudio.com/category/marbel-and-friends-kids-games');
+    });
+    Route::get('/riri-story-books', function () {
+        return redirect('https://www.educastudio.com/category/riri-story-books');
+    });
+    Route::get('/kolak-kids-songs', function () {
+        return redirect('https://www.educastudio.com/category/kolak-kids-songs');
+    });
+   });
+
+//router parameter
+Route::get('/news', function () {
+    return redirect('https://www.educastudio.com/news');
+ 
+});
+
+Route::get('/news/{id}', function ($id) {
+    return 'https://www.educastudio.com/news/educa-studio-berbagi-untuk-warga-sekitar-terdampak-covid-19 '.$id;
+   });
+
+//router prefix
+Route::prefix('/program')->group(function () {
+    Route::get('/karir', function () {
+        return redirect('https://www.educastudio.com/program/karir');
+    });
+    Route::get('/magang', function () {
+        return redirect('https://www.educastudio.com/program/magang');
+    });
+    Route::get('/kunjungan-industri', function () {
+        return redirect('https://www.educastudio.com/program/kunjungan-industri');
+    });
+});    
+
+//router biasa
+Route::get('/about-us', function () {
+    return redirect('https://www.educastudio.com/about-us');
+ 
+});
+
+//route resource only
+Route::resource('/contact-us', ContactController::class)->only([
+    'index'
+   ]);
